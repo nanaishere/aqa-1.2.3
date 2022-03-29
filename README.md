07d574ef684ddf0e713c6f97dc424dac2ba0618a
nanaishere <naiavasilyeva@gmail.com>
2022-03-29 13:35:43 +0300

    V1

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/6v82beg2ixk98jba/branch/master?svg=true)](https://ci.appveyor.com/project/l0197d/aqa-1-2-3/branch/master)
+
---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,29 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+class postmanEchoTest {
+
+    @Test
+    void shouldReturnData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
+
